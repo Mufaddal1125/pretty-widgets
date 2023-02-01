4bbaee10ea45493c2b846773fe5279e677f7641f
Mufaddal Patanwala <65411218+Mufaddal1125@users.noreply.github.com>
2023-02-01 09:51:06 +0400

    add context extensions

---
+++ lib/src/context_extensions.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => theme.textTheme;
+  ColorScheme get colorScheme => theme.colorScheme;
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+  Size get size => mediaQuery.size;
+  double get width => size.width;
+  double get height => size.height;
+  EdgeInsets get padding => mediaQuery.padding;
+  TextDirection get textDirection => Directionality.of(this);
+  bool get isDark => colorScheme.brightness == Brightness.dark;
+  bool get isLight => colorScheme.brightness == Brightness.light;
+  bool get isRTL => textDirection == TextDirection.rtl;
+  bool get isLTR => textDirection == TextDirection.ltr;
+  bool get isPortrait => size.width < size.height;
+  bool get is12HourFormat => !mediaQuery.alwaysUse24HourFormat;
+  bool get is24HourFormat => mediaQuery.alwaysUse24HourFormat;
+  bool get isSmall => size.width < 600;
+  bool get isMedium => size.width >= 600 && size.width < 960;
+  bool get isLarge => size.width >= 960 && size.width < 1280;
+  bool get isXLarge => size.width >= 1280 && size.width < 1920;
+  bool get isXXLarge => size.width >= 1920;
+
+  Color get primaryColor => theme.primaryColor;
+  Color get primaryColorDark => theme.primaryColorDark;
+  Color get primaryColorLight => theme.primaryColorLight;
+  Color get backgroundColor => theme.colorScheme.background;
+  Color get scaffoldBackgroundColor => theme.scaffoldBackgroundColor;
+  Color get canvasColor => theme.canvasColor;
+  Color get cardColor => theme.cardColor;
+  Color get dividerColor => theme.dividerColor;
+  Color get focusColor => theme.focusColor;
+  Color get hoverColor => theme.hoverColor;
+  Color get highlightColor => theme.highlightColor;
+  Color get splashColor => theme.splashColor;
+  Color get unselectedWidgetColor => theme.unselectedWidgetColor;
+  Color get disabledColor => theme.disabledColor;
+
+  TextStyle? get displayLarge => textTheme.displayLarge;
+  TextStyle? get displayMedium => textTheme.displayMedium;
+  TextStyle? get displaySmall => textTheme.displaySmall;
+  TextStyle? get headlineMedium => textTheme.headlineMedium;
+  TextStyle? get headlineSmall => textTheme.headlineSmall;
+  TextStyle? get titleLarge => textTheme.titleLarge;
+  TextStyle? get titleMedium => textTheme.titleMedium;
+  TextStyle? get titleSmall => textTheme.titleSmall;
+  TextStyle? get bodyLarge => textTheme.bodyLarge;
+  TextStyle? get bodyMedium => textTheme.bodyMedium;
+  TextStyle? get bodySmall => textTheme.bodySmall;
+  TextStyle? get labelLarge => textTheme.labelLarge;
+  TextStyle? get labelSmall => textTheme.labelSmall;
+}
